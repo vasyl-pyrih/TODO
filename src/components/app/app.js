358ec25a542f89b8712de742954ee7ddc5008c22
pyrigok <pyrigok@i.ua>
2019-07-24 16:45:13 +0300

    app component added

---
+++ src/components/app/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+
+import './app.css';
+
+
+const App = () => {
+
+    const todoData = [
+        {label: 'python', important: false, id: 1},
+        {label: 'js', important: false, id: 2},
+        {label: 'react', important: true, id: 3}
+    ]
+
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <ItemStatusFilter />
+            <TodoList todos={todoData} />
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemStatusFilter from './components/item-status-filter';
+import App from './components/app';
 
-
-const App = () => {
-
-    const todoData = [
-        {label: 'python', important: false, id: 1},
-        {label: 'js', important: false, id: 2},
-        {label: 'react', important: true, id: 3}
-    ]
-
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <ItemStatusFilter />
-            <TodoList todos={todoData} />
-        </div>
-    )
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
