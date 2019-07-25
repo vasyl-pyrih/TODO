6929b1a347bc0789b6c7243ffa6f3eb5a0de001e
pyrigok <pyrigok@i.ua>
2019-07-25 16:34:53 +0300

    important and done states change added

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -13,15 +13,19 @@ export default class TodoListItem extends Component {
         };
 
         this.onLabelClick = () => {
-            this.setState({
-                done: true
+            this.setState(({done}) => {
+                return {
+                    done: !done
+                };
             })
         };
 
         this.onMarkImportant = () => {
-            this.setState({
-                important: true
-            })
+            this.setState((state) => {
+                return {
+                    important: !state.important
+                };
+            });
         }
 
     }
