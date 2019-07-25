94324d670df6a37767cac3df7a52e5acd2d2b794
pyrigok <pyrigok@i.ua>
2019-07-25 18:10:44 +0300

    delete func added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -8,22 +8,49 @@ import ItemStatusFilter from '../item-status-filter';
 import './app.css';
 
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-        {label: 'python', important: false, id: 1},
-        {label: 'js', important: false, id: 2},
-        {label: 'react', important: true, id: 3}
-    ]
+    constructor() {
+        super();
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <SearchPanel />
-            <ItemStatusFilter />
-            <TodoList todos={todoData} />
-        </div>
-    )
+        this.state = {
+            todoData: [
+                {label: 'python', important: false, id: 1},
+                {label: 'js', important: false, id: 2},
+                {label: 'react', important: true, id: 3}
+            ]
+        }
+
+    }
+
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+
+            const idx = todoData.findIndex((el) => el.id === id);
+            
+            const newTodoData = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx + 1)
+            ];
+
+            return {
+                todoData: newTodoData
+            }
+
+        });
+    }
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+                <SearchPanel />
+                <ItemStatusFilter />
+                <TodoList 
+                    todos={this.state.todoData}
+                    onDeleted={ this.deleteItem } />
+            </div>
+        )
+    }
+
 }
-
-export default App;
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -33,7 +33,7 @@ export default class TodoListItem extends Component {
  
     render () {
 
-        const  { label } = this.props;
+        const  { label, onDeleted } = this.props;
         const { done, important } = this.state;
 
         let classNames = 'todo-list-item';
@@ -63,8 +63,9 @@ export default class TodoListItem extends Component {
                 </button>
     
                 <button type="button"
-                        className="btn btn-outline-danger btn-sm float-right">
-                <i className="fa fa-trash-o" />
+                        className="btn btn-outline-danger btn-sm float-right"
+                        onClick={onDeleted}>
+                    <i className="fa fa-trash-o" />
                 </button>
                 
             </span>
