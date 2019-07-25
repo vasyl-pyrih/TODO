bc318c55829d6c36f49552cce617130e74cf8b5f
pyrigok <pyrigok@i.ua>
2019-07-25 14:50:08 +0300

    setState to list item and onClick to exclamation button added.

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -8,7 +8,8 @@ export default class TodoListItem extends Component {
         super();
 
         this.state = {
-            done: false
+            done: false,
+            important: false
         };
 
         this.onLabelClick = () => {
@@ -17,36 +18,43 @@ export default class TodoListItem extends Component {
             })
         };
 
+        this.onMarkImportant = () => {
+            this.setState({
+                important: true
+            })
+        }
+
     }
 
  
     render () {
 
-        const  { label, important = false } = this.props;
-        const { done } = this.state;
+        const  { label } = this.props;
+        const { done, important } = this.state;
 
         let classNames = 'todo-list-item';
+
         if (done) {
             classNames += ' done';
         }
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
-    
+        if (important) {
+            classNames += ' important';
+        }
+
+ 
         return (
             <span className={classNames}>
 
                 <span
                     className="todo-list-item-label"
-                    style={style}
                     onClick={ this.onLabelClick } >
                     { label }
                 </span>    
             
                 <button type="button"
-                        className="btn btn-outline-success btn-sm float-right">
+                        className="btn btn-outline-success btn-sm float-right"
+                        onClick={ this.onMarkImportant }>
                     <i className="fa fa-exclamation" />
                 </button>
     
