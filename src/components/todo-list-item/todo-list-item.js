b48da3623c6aeb453ff9dd96b3dd7f08f443536f
pyrigok <pyrigok@i.ua>
2019-07-25 14:40:52 +0300

    state to list item added

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -7,16 +7,28 @@ export default class TodoListItem extends Component {
     constructor() {
         super();
 
-        this.onLabelClick = () => {
-            console.log(`${this.props.label}`); 
+        this.state = {
+            done: false
         };
-        
+
+        this.onLabelClick = () => {
+            this.setState({
+                done: true
+            })
+        };
+
     }
 
  
     render () {
 
         const  { label, important = false } = this.props;
+        const { done } = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
 
         const style = {
             color: important ? 'steelblue' : 'black',
@@ -24,7 +36,7 @@ export default class TodoListItem extends Component {
         };
     
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
 
                 <span
                     className="todo-list-item-label"
