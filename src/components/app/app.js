a73e0aea714ee049e22224b285c6699e9c065c7c
pyrigok <pyrigok@i.ua>
2019-07-29 12:04:57 +0300

    onToggleProperty added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -75,29 +75,40 @@ export default class App extends Component {
         });
     }
 
+    toggleProperty(arr, id, propName) {
+        const idx = arr.findIndex((el) => el.id === id);
+            
+            // 1. update object
+            const oldItem = arr[idx];
+            const newItem = {...oldItem, [propName]: !oldItem[propName]};
+
+            // 2. constarust new array
+            return [
+                ...arr.slice(0, idx),
+                newItem,
+                ...arr.slice(idx + 1)
+            ];
+
+    }
+
     onToggleImportant = (id) => {
-        console.log('Important', id);
+
+        this.setState(({ todoData }) => {
+           
+            return {
+                todoData: this.toggleProperty(todoData, id, 'important')
+            };
+
+        });
     }
 
     onToggleDone = (id) => {
 
         this.setState(({ todoData }) => {
-            const idx = todoData.findIndex((el) => el.id === id);
-            
-            // 1. update object
-            const oldItem = todoData[idx];
-            const newItem = {...oldItem, done: !oldItem.done}
-
-            // 2. constarust new array
-            const newArray = [
-                ...todoData.slice(0, idx),
-                newItem,
-                ...todoData.slice(idx + 1)
-            ];
-
+           
             return {
-                todoData: newArray
-            }
+                todoData: this.toggleProperty(todoData, id, 'done')
+            };
 
         });
        
@@ -105,11 +116,11 @@ export default class App extends Component {
 
     render() {
 
-        const doneCount = this.state.todoData
-                        .filter((el) => el.done).length;
+        const { todoData } = this.state;
 
-        const todoCount = this.state.todoData.length - doneCount;
-        console.log(this.state.todoData.lenght);
+        const doneCount = todoData.filter((el) => el.done).length;
+
+        const todoCount = todoData.length - doneCount;
 
         return (
             <div className="todo-app">
@@ -117,7 +128,7 @@ export default class App extends Component {
                 <SearchPanel />
                 <ItemStatusFilter />
                 <TodoList 
-                    todos={ this.state.todoData }
+                    todos={ todoData }
                     onDeleted={ this.deleteItem } 
                     onToggleImportant={ this.onToggleImportant }
                     onToggleDone={ this.onToggleDone }    
